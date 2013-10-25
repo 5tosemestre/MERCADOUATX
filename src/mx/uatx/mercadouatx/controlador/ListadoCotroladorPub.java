c497359d96ce72294f61710dd1523cf8cc73d38a
joseAlvarado <josealvarado1027@hotmail.com>
2013-10-25 14:49:26 -0500

    ordena publicasiones
    ListadoControladorPub enlista en orden las publucasiones del
    usuario que esta en sección.

--- src/mx/uatx/mercadouatx/controlador/ListadoCotroladorPub.java
+++ src/mx/uatx/mercadouatx/controlador/ListadoCotroladorPub.java
@@ -30,7 +30,10 @@ public class ListadoCotroladorPub {
 		CapturaPublicADO captura = new CapturaPublicADO();
 		
 		listar= captura.listarpublica();
-					
+		
+	
+		
+		
 	}
 	
 	public String obtenerValorSesion(String clave){
@@ -47,7 +50,6 @@ public class ListadoCotroladorPub {
 	public List<PublicacionTO> getListar() {
 		return listar;
 	}
-	
 	public void setListar(List<PublicacionTO> listar) {
 		this.listar = listar;
 	}
